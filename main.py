2227f254e35a800cb5adddb981fb431a5b6c8b72
Marco Lindner <marco.lindner@gmx.org>
2023-10-22 00:09:30 +0200

    Adding Python Code Formatter

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def root():
     return "Hello World"
 
+
 @app.get("/todos")
 def get_todos():
     return "Get Todo"
+
 
 @app.put("/todo")
 def put_todos():
